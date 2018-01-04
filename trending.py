2d6b08d75e0184e68fbd719999b33929a1671e73
Ubuntu <jlin423@jonathanmedium.0qz4zeomjxke1cpki53vdupz3c.jx.internal.cloudapp.net>
2018-01-04 19:49:42 +0000

    ok

--- trending.py
+++ trending.py
@@ -6,11 +6,32 @@ import urllib
 import sys
 import traceback
 import redis
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 crypto_api = 'https://min-api.cryptocompare.com/data'
 sms_api = 'https://api.clockworksms.com/http/send.aspx'
 sms_key = '3270b901e913f2a0072b2bc20fe15de80cac0083'
 phones = ['15102008156','19167058495']
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 def log_unhandled_exception(*exc_info):
    text = "".join(traceback.format_exception(*exc_info))
@@ -20,7 +41,7 @@ def log_unhandled_exception(*exc_info):
 # Gets the top 100 coins from coinmarketcap
 def get_top_100():
     coins = []
-    res = requests.get('https://coinmarketcap.com/')
+    res = requests_retry_session().get('https://coinmarketcap.com/')
     p = re.compile('<span class="currency-symbol"><a href="/currencies/.*?/">(.*?)<')
     for m in p.finditer(res.text):
         coins.append(m.group(1))
@@ -43,45 +64,56 @@ def fix_coin_symbols(coins):
     return coins
 
 # A coin is trending up if volume is significantly above average and spikes in price
-def is_trending(sym, data, vol_threshold=3, price_threshold=1.1):    
-    op,cl,vol,avg_vol = data['open'],data['close'],data['curr_vol'],data['avg_vol']
+def is_trending(sym, data, vol_threshold=3, price_threshold=1.15):    
+    op,cl,vol,avg_vol,hi,lo = data['open'],data['close'],data['curr_vol'],data['avg_vol'],data['hi'],data['lo']
     if avg_vol==0:        
         logging.warning('Volume is 0 for %s', sym)
         vol,avg_vol = vol_threshold,1 # ignore volume 
-    if op==0:
+    if lo==0:
         logging.error('Price is 0 for %s', sym)
         return False
-    if cl/op >= price_threshold and vol/avg_vol >= vol_threshold:
+    if hi/lo >= price_threshold and vol/avg_vol >= vol_threshold:
         return True
     return False
 
 def notify(s):
     p = ','.join(phones)
-    res = requests.get('{0}?key={1}&to={2}&content={3}'.format(sms_api,sms_key,p,urllib.parse.quote_plus(s)))
+    res = requests_retry_session().get('{0}?key={1}&to={2}&content={3}'.format(sms_api,sms_key,p,urllib.parse.quote_plus(s)))
     if "Error" in res.text:
         logging.warning('Error sending SMS: %s', res.text)
         return
     logging.info('SMS sent to {0}'.format(p))
     
-def get_curr_price(coins):
-    res = requests.get('{0}/price?fsym=BTC&tsyms={1}'.format(crypto_api,coins))                
+def get_curr_prices(coins):
+    res = requests_retry_session().get('{0}/price?fsym=BTC&tsyms={1}'.format(crypto_api,','.join(coins))).json()
+    return res
+
+# Converts decimal price in bitcoin to more readable form
+def satoshi(p):
+    return int(p*10**7)
 
 # Configure logging
-logging.basicConfig(filename='trending.log',level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(filename='trending.log',level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 sys.excepthook = log_unhandled_exception            
             
 coins = get_top_100()
 coins = fix_coin_symbols(coins)
 
+red = redis.Redis()
+
 # Main loop 
 tc = []
 for c in coins:
+    # Skip coin if we've sent notification in last 24hrs
+    if red.exists(c):
+        continue
+        
     if c=='BTC':
         fsym,tsym = 'BTC','USD'
     else:
-        fsym,tsym = c,'BTC'
-        
-    res = requests.get('{0}/histohour?fsym={1}&tsym={2}&limit=80&e=CCCAGG'.format(crypto_api,fsym,tsym)).json()
+        fsym,tsym = c,'BTC'        
+
+    res = requests_retry_session().get('{0}/histohour?fsym={1}&tsym={2}&limit=80&e=CCCAGG'.format(crypto_api,fsym,tsym)).json()
     
     if res['Response'] != 'Success':
         logging.warning('Couldn\'t find symbol %s', fsym)
@@ -92,6 +124,8 @@ for c in coins:
         continue
     
     data = summarize_data(res['Data'])
+    
+    logging.debug(data)
         
     if is_trending(fsym, data):
         tc.append(fsym)
@@ -100,6 +134,13 @@ for c in coins:
     time.sleep(5)
 
 if tc:
-    notify('{0} trending'.format(','.join(tc)))
+    # Get the current prices
+    cp = get_curr_prices(tc)
+    s = ', '.join('{0} ({1})'.format(c,satoshi(1/cp[c])) for c in tc)
+    notify('{0} trending'.format(s))
+    
+    # Remember coins notified already
+    for c in tc:
+        red.set(c, cp[c], ex=86400)
     
     
